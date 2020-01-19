ddc6ef6756c55d6d915c5c218265babf717107e1
PulkitKhagta <khagtapulkit@gmail.com>
2020-01-19 17:15:45 +0530

    Topsis Ranker package

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Topsis-ranker", # Replace with your own username
-    version="1.0.0",
+    name="Topsis-Ranker", # Replace with your own username
+    version="1.0.2",
     author="Pulkit Khagta",
     author_email="khagtapulkit@gmail.com",
     description="A python package to get topsis rank of given dataset",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/PulkitKhagta/Topsis-Pyhton-Package",
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-    python_requires='>=3.6',
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "topsis = topsis.TOPSIS3410:main",
+        ]
+    },
 )
